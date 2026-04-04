f5323b9de4291b271b22052a2c5540512ffda1dc
hyungjinkim500 <hyungjinkim500@gmail.com>
2026-04-04 12:18:10 +0000

    style: 로고 및 캐치프라이즈 애니메이션 적용

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore, doc, getDoc, collection, query, where, orderBy, limit, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// Your web app's Firebase configuration
+// Your web app\'s Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAubNxnFjJrgh1WVfsy8Xc4-fpL5WPoSTY",
   authDomain: "dddc-hyungjin-0726.firebaseapp.com",
   projectId: "dddc-hyungjin-0726",
   storageBucket: "dddc-hyungjin-0726.firebasestorage.app",
   messagingSenderId: "308736358493",
-  appId: "1:308736358493:web:ca6cf156337c04ff5835b4"
+  appId: "1:308736358493:web:ca6cf156337c04ff5835b4",
+  measurementId: "G-Z333VZ22WB"
 };
 
 // Initialize Firebase
